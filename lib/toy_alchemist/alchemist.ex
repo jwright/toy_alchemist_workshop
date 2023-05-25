602c9667bb7ae87984434c435a1f8c1e184689e4
Jamie Wright <jamie@brilliantfantastic.com>
2023-05-25 18:06:07 +0200

    Alchemist can now move in all directions

--- lib/toy_alchemist/alchemist.ex
+++ lib/toy_alchemist/alchemist.ex
@@ -1,14 +1,14 @@
 defmodule ToyAlchemist.Alchemist do
-  defstruct [:position]
+  defstruct [:facing, :position]
 
   alias ToyAlchemist.Position
 
-  def move(%__MODULE__{position: position} = alchemist, direction)
+  def move(%__MODULE__{position: position, facing: direction} = alchemist)
       when direction in [:east, :west, :north, :south] do
     %{alchemist | position: Position.move(position, direction)}
   end
 
-  def new(position \\ Position.new()) do
-    struct!(__MODULE__, position: position)
+  def new(position \\ Position.new(), facing \\ :north) do
+    struct!(__MODULE__, facing: facing, position: position)
   end
 end
--- test/toy_alchemist/alchemist_test.exs
+++ test/toy_alchemist/alchemist_test.exs
@@ -9,19 +9,27 @@ defmodule ToyAlchemist.AlchemistTest do
     [alchemist: alchemist]
   end
 
-  test "move\2 moves west when the alchemist needs to go west", %{alchemist: alchemist} do
-    assert Alchemist.move(alchemist, :west).position == %Position{north: 0, east: 1}
+  test "move\1 moves west when the alchemist is facing west", %{alchemist: alchemist} do
+    alchemist = %{alchemist | facing: :west}
+
+    assert Alchemist.move(alchemist).position == %Position{north: 0, east: 1}
   end
 
-  test "move\2 moves east when the alchemist needs to go east", %{alchemist: alchemist} do
-    assert Alchemist.move(alchemist, :east).position == %Position{north: 0, east: 3}
+  test "move\1 moves east when the alchemist is facing east", %{alchemist: alchemist} do
+    alchemist = %{alchemist | facing: :east}
+
+    assert Alchemist.move(alchemist).position == %Position{north: 0, east: 3}
   end
 
-  test "move\2 moves north when the alchemist needs to go north", %{alchemist: alchemist} do
-    assert Alchemist.move(alchemist, :north).position == %Position{north: 1, east: 2}
+  test "move\1 moves north when the alchemist is facing north", %{alchemist: alchemist} do
+    alchemist = %{alchemist | facing: :north}
+
+    assert Alchemist.move(alchemist).position == %Position{north: 1, east: 2}
   end
 
-  test "move\2 moves south when the alchemist needs to go south", %{alchemist: alchemist} do
-    assert Alchemist.move(alchemist, :south).position == %Position{north: -1, east: 2}
+  test "move\1 moves south when the alchemist is facing south", %{alchemist: alchemist} do
+    alchemist = %{alchemist | facing: :south}
+
+    assert Alchemist.move(alchemist).position == %Position{north: -1, east: 2}
   end
 end
